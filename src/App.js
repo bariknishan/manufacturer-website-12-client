0f7481d2c9cdfd21c263b0dab2fa7b0fad75a022
bariknishan <barik6286@gmail.com>
2022-05-22 01:56:51 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Myprofile from './Pages/About/Myprofile';
+import Homepage from './Pages/Home/Homepage';
+import Navbar from './Pages/SharedPages/Navbar';
 
 function App() {
   return (
     <div className="App">
    
-<h2>Wellcome</h2>
+          <Navbar></Navbar>
+
+ <Routes>
+
+   <Route path="/" element={<Homepage></Homepage>}></Route>
+   <Route path="myprofile" element={<Myprofile></Myprofile>}></Route>
+ </Routes>
+
+
+
+
+
+
 
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,23 @@ module.exports = {
   theme: {
     extend: {},
   },
+  
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "white",
+      "cupcake",
+    ],
+  },
+
+
   plugins: [require("daisyui")],
 }

ff92907dd47cbaed36dd5fb58586c3bf3c51f445
bariknishan <barik6286@gmail.com>
2022-05-24 02:13:31 +0600

    react hook form added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,9 +3,14 @@ import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../firebase.init'
+import { useForm } from "react-hook-form";
+
 const Login = () => {
 
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    // react hook forms
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
 
     if (user) {
         console.log(user)
@@ -25,29 +30,57 @@ const Login = () => {
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-accent-300 p-12">
                     <h2 className='text-xl font-bold' >Please Log In here</h2>
                     <div className="card-body">
+                        <form onSubmit={handleSubmit()}>
+
+                       
+                     
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" />
+                            <input type="email" placeholder="email" className="input input-bordered" 
+                            
+                            {...register("email",
+                            {
+                               pattern: {
+                                   value:  /[A-Za-z]{3}/,
+                                   message: 'error message'
+                               }
+                                })}
+                            
+                            />
                         </div>
+                        
+
+                       {/* password frild  */}
+
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="text" placeholder="password" className="input input-bordered"
+                         
+                            
+                            />
 
                         </div>
+
+                            <input {...register("lastName", { required: true })} />
+                            {errors.lastName && "Last name is required"}
+
+                        
+                        </form>
+
                         <div className="form-control mt-4">
                             <button className="btn btn-primary">Login</button>
                         </div>
-                         <p>New Here? </p>
-                         <p> <Link to="/signup" className='text-green-400 font-bold' >Create New Account</Link></p>
+                        <p>New Here? </p>
+                        <p> <Link to="/signup" className='text-GREEN-400 font-bold' >Create New Account</Link></p>
                         <div className="divider">OR</div>
 
                         <div className="form-control mt-4">
 
-                            <button onClick={()=>signInWithGoogle()} className="btn btn-secondary">Continue With Google</button>
+                            <button onClick={() => signInWithGoogle()} className="btn btn-secondary">Continue With Google</button>
                         </div>
 
                     </div>

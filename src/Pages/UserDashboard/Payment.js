172706e1a345524b844dd516473dedfb588f1554
bariknishan <barik6286@gmail.com>
2022-05-28 10:53:28 +0600

    stripe added

--- src/Pages/UserDashboard/Payment.js
+++ src/Pages/UserDashboard/Payment.js
@@ -1,8 +1,12 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../SharedPages/Loading';
+import CheckoutForm from './CheckoutForm';
 
+const stripePromise = loadStripe('pk_test_51L490bAu7VDKHv9Htsk8RJRscacdXy77OZgiRKkTorAHEphHr7Dr0Tjm8rdK2kavLF6qJpCHmyHHk5uFzVRDbIJR00UilsbNj2');
 const Payment = () => {
 
     const { id } = useParams() ////// id purpose
@@ -22,17 +26,19 @@ const Payment = () => {
 
     return (
         < div>
-            <div class="card w-50 bg-primary max-w-md   text-primary-content">
-                <div class="card-body">
+            <div className="card w-50 bg-primary max-w-md   text-primary-content">
+                <div className="card-body">
                     <h2 className='text-white-500'>Hello! Mr {order.buyer}</h2>
-                    <h2 class="card-title">pay for {order.itemPackage}</h2>
+                    <h2 className="card-title">pay for {order.itemPackage}</h2>
                     <p> we will delever it on <span className='text-secondary'> {order.date}</span></p>
-                     <p> You should pay ${order.price}</p>
+                    <p> You should pay ${order.price}</p>
                 </div>
             </div>
-            <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
-                <div class="card-body">
-
+            <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <Elements stripe={stripePromise}>
+                   <CheckoutForm></CheckoutForm>
+                    </Elements>
                 </div>
             </div>
         </div>
